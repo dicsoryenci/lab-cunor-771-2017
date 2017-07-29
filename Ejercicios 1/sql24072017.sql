666a98ebda41b8cc08e5859b6db9b3f2427375af
Geovani de León <erwingeo1021@gmail.com>
2017-07-29 11:42:45 -0600

    Se añadió primeros ejemplos de consultas y modelo ER

---
+++ Ejercicios 1/sql24072017.sql
@@ -0,0 +1,24 @@
+﻿SELECT p.cod_producto AS codprod, p.nom_producto AS producto
+FROM dbo.producto AS p 
+
+
+
+--WHERE fec_caducidad BETWEEN '2017-07-01' AND '2017-07-30'
+WHERE fec_caducidad BETWEEN CURRENT_DATE AND (CURRENT_DATE +10)
+
+
+SELECT pro.nom_proveedor, p.nom_producto
+FROM dbo.producto AS p 
+INNER JOIN dbo.proveedor AS pro ON p.cod_proveedor = pro.cod_proveedor
+WHERE p.estado_activo = 0 AND p.fec_caducidad BETWEEN CURRENT_DATE AND (CURRENT_DATE +10)
+
+=
+>
+<
+!=
+<=
+>=
+LIKE 
+BETWEEN
+
+INNER JOIN
