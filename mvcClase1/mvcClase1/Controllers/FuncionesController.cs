9a2e81cc5f4203721cbfd3158f1580034f80aa7d
Geovani de León <erwingeo1021@gmail.com>
2017-08-30 13:00:30 -0600

    -- Cambios en la vista índice.

---
+++ mvcClase1/mvcClase1/Controllers/FuncionesController.cs
@@ -0,0 +1,31 @@
+﻿using mvcClase1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcClase1.Controllers
+{
+    public class FuncionesController : Controller
+    {
+        List<Personas> lstPersonas = new List<Personas>();
+        // GET: Funciones
+        public ActionResult Index()
+        {
+            nuevaPersona("Julio", "Archila");
+            nuevaPersona("Carlos", "Muñoz");
+            nuevaPersona("Juan", "Pancho");
+            ViewBag.personas = lstPersonas;
+            return View();
+        }
+
+
+        private void nuevaPersona(string nombres, string apellidos) {
+            Personas persona = new Personas();
+            persona.nombres = nombres;
+            persona.apellidos = apellidos;
+            lstPersonas.Add(persona);
+        }
+    }
+}
---
+++ mvcClase1/mvcClase1/Models/Personas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcClase1.Models
+{
+    public class Personas
+    {
+        public string nombres;
+        public string apellidos;
+    }
+}
